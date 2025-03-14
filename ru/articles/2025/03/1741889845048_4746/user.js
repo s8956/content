e70a542fd6a1755a8de6d8496567ebde87c53bd5
KaiKimera <git@kai.kim>
2025-03-14 13:43:31 +0000

    2025-03-14 13:43:31

--- ru/articles/2025/03/1741889845048_4746/user.js
+++ ru/articles/2025/03/1741889845048_4746/user.js
@@ -1,8 +1,10 @@
+user_pref("app.donation.eoy.version.viewed", 9999);
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("font.language.group", "x-cyrillic");
 user_pref("mail.biff.play_sound", false);
 user_pref("mail.chat.play_sound", false);
 user_pref("mail.default_send_format", 1);
+user_pref("mail.rights.version", 1);
 user_pref("mail.serverDefaultStoreContractID", "@mozilla.org/msgstore/maildirstore;1");
 user_pref("mailnews.start_page.enabled", false);
 user_pref("messenger.startup.action", 0);
