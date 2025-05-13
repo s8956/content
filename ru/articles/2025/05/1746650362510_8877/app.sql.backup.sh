f5008641b7140bee1dfaf64a7a46f3bce08dbdcb
KaiKimera <git@kai.kim>
2025-05-13 16:40:09 +0300

    Update article

--- ru/articles/2025/05/1746650362510_8877/app.sql.backup.sh
+++ ru/articles/2025/05/1746650362510_8877/app.sql.backup.sh
@@ -47,9 +47,11 @@ run() {
 
 sql_backup() {
   local id; id="$( _id )"
+
   for i in "${SQL_DB[@]}"; do
     local ts; ts="$( _timestamp )"
     local file; file="${i}.${id}.${ts}.sql"
+
     { [[ ! -d "${SQL_DATA}" ]] && mkdir -p "${SQL_DATA}"; } && cd "${SQL_DATA}" \
       && _dump "${i}" "${file}" && _pack "${file}"
   done
@@ -88,34 +90,46 @@ _timestamp() {
 }
 
 _dump() {
+  local db; db="${1}"
+  local file; file="${2}"
+
   case "${SQL_TYPE}" in
-    'mysql') _mysql "${1}" "${2}" ;;
-    'pgsql') _pgsql "${1}" "${2}" ;;
+    'mysql') _mysql "${db}" "${file}" ;;
+    'pgsql') _pgsql "${db}" "${file}" ;;
     *) echo >&2 'SQL_TYPE does not exist!'; exit 1 ;;
   esac
 }
 
 _mysql() {
+  local db; db="${1}"
+  local file; file="${2}"
   local cmd; cmd='mariadb-dump'; [[ $( command -v 'mysqldump' ) ]] && cmd='mysqldump'
+
   "${cmd}" --host="${SQL_HOST:-127.0.0.1}" --port="${SQL_PORT:-3306}" \
     --user="${SQL_USER:-root}" --password="${SQL_PASS}" \
-    --single-transaction --skip-lock-tables "${1}" --result-file="${2}"
+    --single-transaction --skip-lock-tables "${db}" --result-file="${file}"
 }
 
 _pgsql() {
+  local db; db="${1}"
+  local file; file="${2}"
+
   pg_dump --host="${SQL_HOST:-127.0.0.1}" --port="${SQL_PORT:-5432}" \
     --username="${SQL_USER:-postgres}" --no-password \
-    --dbname="${1}" --file="${2}" \
+    --dbname="${db}" --file="${file}" \
     --clean --if-exists --no-owner --no-privileges --quote-all-identifiers
 }
 
 _pack() {
-  xz "${1}" && { [[ -f "${1}" ]] && rm -f "${1}"; }
+  local file; file="${1}"
+
+  xz "${file}" && { [[ -f "${file}" ]] && rm -f "${file}"; }
 }
 
 _mail() {
   local id; id="#ID:$( hostname -f ):$( dmidecode -s system-uuid )"
   local type; type="#TYPE:BACKUP:${3}"
+
   printf '%s\n\n-- \n%s\n%s' "${2}" "${id^^}" "${type^^}" | mail -s "${1}" "${MAIL_TO}"
 }
 
